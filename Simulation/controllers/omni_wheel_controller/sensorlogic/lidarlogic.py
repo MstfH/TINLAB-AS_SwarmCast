dcec4fa87bc645367d93f3c6bbff8cacada73336
MstfH <mo.mh43@gmail.com>
2020-05-14 15:54:30 +0200

    Able to use values from lidar

---
+++ Simulation/controllers/omni_wheel_controller/sensorlogic/lidarlogic.py
@@ -0,0 +1,12 @@
+'''
+900 values
+number of values = first value in the array-last value in the array
+117 = 169-286left
+130 = 394-524front
+117 = 619-735right
+117 = 0-61 & 843-899back
+back is the way the lidar line points to see use Ctrl+F7
+'''
+def lidarPoint(lidarValue):
+    #An array with 900 values
+    print(lidarValue)
