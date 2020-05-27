5aa3bda6cbca79b925f355b2906295e407098c47
unknown <qhoogwerf@gmail.com>
2020-05-27 15:33:40 +0200

    moved states to parent for enum

---
+++ Simulation/controllers/stateDefs.py
@@ -0,0 +1,17 @@
+from enum import Enum, auto
+
+class ServerState(Enum):
+    WAITING_FOR_CONNECTIONS = auto()
+    CALCULATING_OPTIMAL_ASSIGNMENT = auto()
+    WAITING_FOR_FORMATION = auto()
+    ASSIGNING_COLORS = auto()
+    DONE = auto()
+
+class BotState(Enum):
+    #IDLE = "I"
+    IDLE = auto()
+    TRAVELLING_NORTH = auto()
+    TRAVELLING_EAST = auto()
+    TRAVELLING_SOUTH = auto()
+    TRAVELLING_WEST = auto()
+    IN_FORMATION = auto()
